0bcfe97ed722a45374dbf2df59dcef7594d31fc9
designer <ganlanyuan@gmail.com>
2015-02-26 12:28:09 -0500

    add css slider

---
+++ docs/test.php
@@ -0,0 +1,66 @@
+<?php include 'include/head.php'; ?>
+<body>
+<div class="page">
+  <div class="container">
+    <div class="slide">
+      <input type="radio" name="slide" id="slide-1" checked="">
+      <input type="radio" name="slide" id="slide-2">
+      <input type="radio" name="slide" id="slide-3">
+      <input type="radio" name="slide" id="slide-4">
+      <input type="radio" name="slide" id="slide-5">
+      <input type="checkbox" name="slide-autoplay" id="slide-autoplay">
+      <div class="slide-container">
+        <ul class="inner">
+          <li>
+            <div class="item">
+              <img src="http://csscience.com/responsiveslidercss3/CouldDragonByBjzaba.png">
+              <div class="info"><h3>Cloud Dragon</h3> by <a href="http://voyager3.tumblr.com">Brendan Zabarauskas</a></div>
+            </div>
+          </li>
+          <li>
+            <div class="item">
+              <img src="http://csscience.com/responsiveslidercss3/MountainFortByBjzaba.png">
+              <div class="info"><h3>Mountain Fort</h3> by <a href="http://voyager3.tumblr.com">Brendan Zabarauskas</a></div>
+            </div>
+          </li>
+          <li>
+            <div class="item">
+              <img src="http://csscience.com/responsiveslidercss3/MountainOutpostByBjzaba.png">
+              <div class="info"><h3>Mountain Outpost</h3> by <a href="http://voyager3.tumblr.com">Brendan Zabarauskas</a></div>
+            </div>
+          </li>
+          <li>
+            <div class="item">
+              <img src="http://csscience.com/responsiveslidercss3/CliffsByBjzaba.png">
+              <div class="info"><h3>Cliffs</h3> by <a href="http://voyager3.tumblr.com">Brendan Zabarauskas</a></div>
+            </div>
+          </li>
+          <li>
+            <div class="item">
+              <img src="http://csscience.com/responsiveslidercss3/HillFortByBjzaba.png">
+              <div class="info"><h3>Hill Fort</h3> by <a href="http://voyager3.tumblr.com">Brendan Zabarauskas</a></div>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="controls">
+        <label for="slide-1"><span class="prev"></span><span class="next"></span></label>
+        <label for="slide-2"><span class="prev"></span><span class="next"></span></label>
+        <label for="slide-3"><span class="prev"></span><span class="next"></span></label>
+        <label for="slide-4"><span class="prev"></span><span class="next"></span></label>
+        <label for="slide-5"><span class="prev"></span><span class="next"></span></label>
+      </div>
+      <div class="dots">
+        <label for="slide-1"><span class="normal"></span><span class="active"></span></label>
+        <label for="slide-2"><span class="normal"></span><span class="active"></span></label>
+        <label for="slide-3"><span class="normal"></span><span class="active"></span></label>
+        <label for="slide-4"><span class="normal"></span><span class="active"></span></label>
+        <label for="slide-5"><span class="normal"></span><span class="active"></span></label>
+      </div>
+      <div class="autoplay" style="margin-top: 20px;"><label for="slide-autoplay"><span></span></label></div>
+    </div>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus ad, molestias accusantium enim alias ducimus. Nihil id possimus quas! Molestiae veniam ea, incidunt molestias a eligendi necessitatibus consectetur voluptatum recusandae.</p>
+  </div>
+</div>
+</body>
+</html>
