6bd9ec4375be9d75f9e92f073a651660a3fa4c41
designer <ganlanyuan@gmail.com>
2015-02-27 16:55:04 -0500

    add autoplay to slider-carousel

--- docs/test.php
+++ docs/test.php
@@ -8,36 +8,36 @@
       <input type="radio" name="slide" id="slide-3">
       <input type="radio" name="slide" id="slide-4">
       <input type="radio" name="slide" id="slide-5">
-      <input type="checkbox" name="slide-autoplay" id="slide-autoplay">
+      <input type="checkbox" name="slide-autoplay" id="slide-autoplay" checked="">
       <div class="slider-container">
         <ul class="inner">
           <li>
             <div class="item">
-              <a href=""><img src="http://csscience.com/responsiveslidercss3/CouldDragonByBjzaba.png"></a>
+              <a href=""><img src="http://placehold.it/1100x619/97cddd/97cddd"></a>
               <div class="info"><h3>Cloud Dragon</h3> by <a href="http://voyager3.tumblr.com">Brendan Zabarauskas</a></div>
             </div>
           </li>
           <li>
             <div class="item">
-              <a href=""><img src="http://csscience.com/responsiveslidercss3/MountainFortByBjzaba.png"></a>
+              <a href=""><img src="http://placehold.it/1100x619/e3d189/e3d189"></a>
               <div class="info"><h3>Mountain Fort</h3> by <a href="http://voyager3.tumblr.com">Brendan Zabarauskas</a></div>
             </div>
           </li>
           <li>
             <div class="item">
-              <a href=""><img src="http://csscience.com/responsiveslidercss3/MountainOutpostByBjzaba.png"></a>
+              <a href=""><img src="http://placehold.it/1100x619/97cddd/97cddd"></a>
               <div class="info"><h3>Mountain Outpost</h3> by <a href="http://voyager3.tumblr.com">Brendan Zabarauskas</a></div>
             </div>
           </li>
           <li>
             <div class="item">
-              <a href=""><img src="http://csscience.com/responsiveslidercss3/CliffsByBjzaba.png"></a>
+              <a href=""><img src="http://placehold.it/1100x619/b6cc84/b6cc84"></a>
               <div class="info"><h3>Cliffs</h3> by <a href="http://voyager3.tumblr.com">Brendan Zabarauskas</a></div>
             </div>
           </li>
           <li>
             <div class="item">
-              <a href=""><img src="http://csscience.com/responsiveslidercss3/HillFortByBjzaba.png"></a>
+              <a href=""><img src="http://placehold.it/1100x619/eea7d4/eea7d4"></a>
               <div class="info"><h3>Hill Fort</h3> by <a href="http://voyager3.tumblr.com">Brendan Zabarauskas</a></div>
             </div>
           </li>
