76780b35c674f07c8a18179b96f9fed041b8e297
ganlanyuan <linwilliam@Williams-iMac.home>
2015-05-31 23:01:03 -0400

    fix some issues

--- docs/include/more-demos.php
+++ docs/include/more-demos.php
@@ -7,25 +7,22 @@
     <a <?php if ($pagename == 'layout-justify' ) { echo 'class="current"'; } ?> href="layout-justify.php">justify</a>
     <a <?php if ($pagename == 'layout-center' ) { echo 'class="current"'; } ?> href="layout-center.php">center</a>
     <a <?php if ($pagename == 'layout-two-columns' ) { echo 'class="current"'; } ?> href="layout-two-columns.php">two-columns</a>
-    <a <?php if ($pagename == 'layout-debug' ) { echo 'class="current"'; } ?> href="layout-debug.php">debug</a>
   </nav>
-  <h4>slider</h4>
+  <h4>components</h4>
   <nav class="demo-links">
-    <a <?php if ($pagename == 'slider-carousel' ) { echo 'class="current"'; } ?> href="slider-carousel.php">carousel</a>
-    <a <?php if ($pagename == 'slider-gallery' ) { echo 'class="current"'; } ?> href="slider-gallery.php">gallery</a>
-  </nav>
-  <h4>addons</h4>
-  <nav class="demo-links">
-    <a <?php if ($pagename == 'addons-type' ) { echo 'class="current"'; } ?> href="addons-type.php">type</a>
     <a <?php if ($pagename == 'addons-button' ) { echo 'class="current"'; } ?> href="addons-button.php">button</a>
     <a <?php if ($pagename == 'addons-media-list' ) { echo 'class="current"'; } ?> href="addons-media-list.php">media-list</a>
     <a <?php if ($pagename == 'addons-off-canvas' ) { echo 'class="current"'; } ?> href="addons-off-canvas.php">off-canvas</a>
     <a <?php if ($pagename == 'addons-dropdown' ) { echo 'class="current"'; } ?> href="addons-dropdown.php">dropdown</a>
     <a <?php if ($pagename == 'addons-tooltip' ) { echo 'class="current"'; } ?> href="addons-tooltip.php">tooltip</a>
-    <a <?php if ($pagename == 'addons-visibility' ) { echo 'class="current"'; } ?> href="addons-visibility.php">visibility</a>
     <a <?php if ($pagename == 'addons-flex-video' ) { echo 'class="current"'; } ?> href="addons-flex-video.php">flex-video</a>
   </nav>
-  <h4>color</h4>
+  <h4>addons</h4>
+  <nav class="demo-links">
+    <a <?php if ($pagename == 'addons-type' ) { echo 'class="current"'; } ?> href="addons-type.php">type</a>
+    <a <?php if ($pagename == 'addons-visibility' ) { echo 'class="current"'; } ?> href="addons-visibility.php">visibility</a>
+  </nav>
+  <h4>color functions</h4>
   <nav class="demo-links">
     <a <?php if ($pagename == 'color-contrast' ) { echo 'class="current"'; } ?> href="color-contrast.php">contrast</a>
     <a <?php if ($pagename == 'color-adjacent' ) { echo 'class="current"'; } ?> href="color-adjacent.php">adjacent</a>
@@ -34,5 +31,10 @@
     <a <?php if ($pagename == 'color-triad' ) { echo 'class="current"'; } ?> href="color-triad.php">triad</a>
     <a <?php if ($pagename == 'color-rectangle' ) { echo 'class="current"'; } ?> href="color-rectangle.php">rectangle</a>
     <a <?php if ($pagename == 'color-square' ) { echo 'class="current"'; } ?> href="color-square.php">square</a>
+  </nav>
+  <h4>slider</h4>
+  <nav class="demo-links">
+    <a <?php if ($pagename == 'slider-carousel' ) { echo 'class="current"'; } ?> href="slider-carousel.php">carousel</a>
+    <a <?php if ($pagename == 'slider-gallery' ) { echo 'class="current"'; } ?> href="slider-gallery.php">gallery</a>
   </nav>
 </div>
