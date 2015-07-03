c50c3d7d083445612e0b2edb237bd7e399d231e7
ganlanyuan <linwilliam@Williams-iMac.home>
2015-07-03 00:22:36 -0400

    update dropdown demo

--- demos/components-dropdown.php
+++ demos/components-dropdown.php
@@ -9,25 +9,65 @@
         <div class="dropdown-1">
           <span>dropdown: hover</span>
           <ul>
-            <li><a href="">item01</a></li>
-            <li><a href="">item02</a></li>
-            <li><a href="">item03</a></li>
+            <li><a href="">item 01</a></li>
+            <li class="sub"><a href="">item 02 <small>►</small></a>
+              <ul>
+                <li><a href="">subitem 01</a></li>
+                <li><a href="">subitem 02</a></li>
+                <li class="sub"><a href="">subitem 03 <small>►</small></a>
+                  <ul>
+                    <li><a href="">subitem 01</a></li>
+                    <li><a href="">subitem 02</a></li>
+                    <li><a href="">subitem 03</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li><a href="">item 03</a></li>
           </ul>
         </div>              
         <div class="dropdown-2">
           <span>dropdown: </span>
           <input type="checkbox" name="" id="dropdown">
-          <label for="dropdown">▼</label>
+          <label for="dropdown"><small>▼</small></label>
           <ul>
-            <li><a href="">item01</a></li>
-            <li><a href="">item02</a></li>
-            <li><a href="">item03</a></li>
+            <li><a href="">item 01</a></li>
+            <li><a href="">item 02</a></li>
+            <li class="sub-2">
+              <input type="checkbox" name="" id="dropdown-2">
+              <label for="dropdown-2"><small>▼</small></label>
+              <a href="">item 03</a>
+              <ul>
+                <li><a href="">subitem 01</a></li>
+                <li class="sub-2">
+                  <input type="checkbox" name="" id="dropdown-3">
+                  <label for="dropdown-3"><small>▼</small></label>
+                  <a href="">subitem 02</a>
+                  <ul>
+                    <li><a href="">subitem 01</a></li>
+                    <li><a href="">subitem 02</a></li>
+                    <li><a href="">subitem 03</a></li>
+                  </ul>
+                </li>
+                <li><a href="">subitem 03</a></li>
+              </ul>
+            </li>
           </ul>
         </div>              
       </div>
       <pre><code class="language-scss">
-.dropdown-1 { @include dropdown(default); }
-.dropdown-2 { @include dropdown(default scale click); }
+.dropdown-1 { 
+  @include dropdown(default); 
+  .sub { 
+    @include dropdown(default right hover); 
+  }
+}
+.dropdown-2 { 
+  @include dropdown(default scale click); 
+  .sub-2 {
+    @include dropdown(default left scale click);
+  }
+}
       </code></pre>
     </div>
     
