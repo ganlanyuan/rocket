eb9d4131f5519a3a53bfb7a65946f4c9e2de7e0c
ganlanyuan <ganlanyuan@gmail.com>
2015-09-07 16:00:33 -0400

    add charts

---
+++ demos/components-charts.php
@@ -0,0 +1,30 @@
+<?php include 'include/head.php'; ?>
+<body>
+<div class="page">
+  <div class="container">
+
+    <div class="topic">
+      <h2 id=""><span>components: </span>validation</h2>
+      <dl class="charts">
+        <dt><h4>Population of Largest U.S. Cities</h4></dt>
+        <dd class="item-1"><span></span><span></span><span></span></dd>
+        <dd class="item-2"><span></span><span></span><span></span></dd>
+        <dd class="item-3"><span></span><span></span><span></span></dd>
+        <dd class="item-4"><span></span><span></span><span></span></dd>
+      </dl>
+      <pre><code class="language-scss">
+      </code></pre>
+    </div>
+    
+    <?php include "include/more-demos.php" ?>
+  </div>
+  <?php include "include/site-footer.php" ?>
+  <script>
+    window.onload = function () {
+      H5F.setup(document.querySelectorAll(".myform"));
+    }
+  </script>
+</div>
+</body>
+</html>
+
