b248ffe368bab2dcc9f044d5d3a2c9b6c7da31f1
WilliamLin <ganlanyuan@gmail.com>
2015-09-08 18:06:24 -0400

    update charts, fix em & rem

--- demos/components-charts.php
+++ demos/components-charts.php
@@ -4,14 +4,56 @@
   <div class="container">
 
     <div class="topic">
-      <h2 id=""><span>components: </span>validation</h2>
-      <dl class="charts">
-        <dt><h4>Population of Largest U.S. Cities</h4></dt>
-        <dd class="item-1"><span></span><span></span><span></span></dd>
-        <dd class="item-2"><span></span><span></span><span></span></dd>
-        <dd class="item-3"><span></span><span></span><span></span></dd>
-        <dd class="item-4"><span></span><span></span><span></span></dd>
-      </dl>
+      <h2 id=""><span>components: </span>Charts</h2>
+      <div class="charts-wrap">
+        <h4>Population of Largest U.S. Cities</h4>
+        <div class="charts">
+          <ul class="items">
+            <li class="item-1">
+              <strong>Copper</strong>
+              <span>8.9</span>
+              <span>5</span>
+              <span>10.7</span>
+            </li>
+            <li class="item-2">
+              <strong>Silver</strong>
+              <span>10.5</span>
+              <span>10</span>
+              <span>12</span>
+            </li>
+            <li class="item-3">
+              <strong>Gold</strong>
+              <span>19.3</span>
+              <span>16</span>
+              <span>12</span>
+            </li>
+            <li class="item-4">
+              <strong>Platinum</strong>
+              <span>21.45</span>
+              <span>22</span>
+              <span>18</span>
+            </li>
+          </ul>
+          <div class="ticks">
+            <div class="tick-1"><span>0</span></div>
+            <div class="tick-2"><span>3</span></div>
+            <div class="tick-3"><span>6</span></div>
+            <div class="tick-4"><span>9</span></div>
+            <div class="tick-5"><span>12</span></div>
+            <div class="tick-6"><span>15</span></div>
+            <div class="tick-7"><span>18</span></div>
+            <div class="tick-8"><span>21</span></div>
+            <div class="tick-9"><span>24</span></div>
+            <div class="tick-10"><span>27</span></div>
+            <div class="tick-11"><span>30</span></div>
+          </div>
+          <div class="tags">
+            <div class="tag-1">sales</div>
+            <div class="tag-2">expenses</div>
+            <div class="tag-3">profit</div>
+          </div>
+        </div>
+      </div>
       <pre><code class="language-scss">
       </code></pre>
     </div>
@@ -19,11 +61,6 @@
     <?php include "include/more-demos.php" ?>
   </div>
   <?php include "include/site-footer.php" ?>
-  <script>
-    window.onload = function () {
-      H5F.setup(document.querySelectorAll(".myform"));
-    }
-  </script>
 </div>
 </body>
 </html>
