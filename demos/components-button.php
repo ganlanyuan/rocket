75245b6ceee2c422c4127010c036f1fb7cd2a2d9
WilliamLin <ganlanyuan@gmail.com>
2015-09-11 16:40:30 -0400

    add more style to button

--- demos/components-button.php
+++ demos/components-button.php
@@ -9,11 +9,16 @@
         <div class="example-button">
           <span class="normal">normal button</span>
           <a href="" class="radius">radius button</a>
-          <button class="round">round button</button> <br>
-          <button class="highlight">highlight</button>
-          <button class="simple">simple</button>
-          <button class="slide-bg">slide-bg</button>
-          <button class="water-drop">water-drop</button>
+          <span class="round">round button</span> <br>
+          <span class="highlight">highlight</span>
+          <span class="simple">simple</span>
+          <span class="slide-bg">slide-bg</span>
+          <span class="water-drop">water-drop</span>
+          <span class="ujarak">ujarak</span>
+          <span class="wayra">wayra</span>
+          <span class="winona ic-star"><span>winona</span></span>
+          <span class="moema">moema</span>
+          <span class="flash">flash <span></span><span></span><span></span><span></span></span>
         </div>
       </div>
       <pre><code class="language-scss">
