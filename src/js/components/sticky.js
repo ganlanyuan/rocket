045cc339a8252c98dd7a41e3467d4f27784ab7df
ganlanyuan <ganlanyuan@gmail.com>
2015-09-23 23:11:00 -0400

    simplify sticky

--- src/js/components/sticky.js
+++ src/js/components/sticky.js
@@ -2,26 +2,23 @@
 // sticky('.sticky', '.wrapper', 20);
 
 var sticky = function(sticky, stickyP, stkT) {
-	var STKMB = getPxValue(kit(sticky).getCurrentStyle('marginBottom'));
+
+	var stk = kit(sticky),
+	    parent = kit(stickyP),
+			T1,
+			T2,
+			B1,
+			B2;
 
 	var stickyCore = function () {
-		var stk = kit(sticky),
-		    parent = kit(stickyP),
-				T1,
-				T2,
-				B1,
-				B2;
-
 		var winH = kit.win.H(),
 		    stkOT = stk.getTop(),
 				stkH = stk.outerHeight(),
-				stkW = stk.outerWidth(),
-				stkMT = getPxValue(stk.getCurrentStyle('marginTop')),
 				stkMB = getPxValue(stk.getCurrentStyle('marginBottom')),
 				parentOT = parent.getTop(),
 				parentH = parent.outerHeight();
 
-		winScroll(function() {
+		function updateSticky() {
 			(typeof stkT === 'number') ? stkT = stkT: stkT = kit(stkT).outerHeight();
 
 			var winST = kit.win.ST();
@@ -30,61 +27,37 @@ var sticky = function(sticky, stickyP, stkT) {
 			if ((stkH + stkT) > winH) {
 				T1 = B1 = winST + winH;
 				T2 = stkOT + stkH;
-				B2 = parentOT + parentH;
-
-				if (T1 > T2 && B1 <= B2 ) {
-					stk.css({
-						'position': 'fixed',
-						'bottom': 0,
-						'margin-bottom': 0,
-						'width': stkW + 'px',
-					});
-				} else if (B1 > B2){
-					parent.css('position', 'relative');
-					stk.css({
-						'position': 'absolute',
-						'margin-bottom': STKMB + 'px',
-					});
-				} else {
-					parent.css('position', 'static');
-					stk.css({
-						'position': 'static',
-						'bottom': 'auto',
-						'width': 'auto',
-						'margin-bottom': STKMB + 'px',
-					});
-				}
+				B2 = parentOT + parentH - stkMB;
 			// window higher than sticky
 			} else {
 				T1 = winST + stkT;
 				T2 = stkOT;
-				B1 = winST + stkT + stkMT + stkH + stkMB;
+				B1 = winST + stkT + stkH + stkMB;
 				B2 = parentOT + parentH;
-				if (T1 > T2 && B1 <= B2 ) {
-					stk.css({
-						'position': 'relative',
-						'top': T1 - T2 + 'px',
-						// 'bottom': 'auto',
-						// 'width': stkW + 'px',
-					});
-				} else if (B1 > B2){
-					// parent.css('position', 'relative');
-					stk.css({
-						'position': 'relative',
-						'top': parentH - (stkMT + stkH + stkMB) - (stkOT - parentOT) + 'px',
-						// 'bottom': 0,
-					});
-				} else {
-					// parent.css('position', 'static');
-					stk.css({
-						'position': 'static',
-						'top': 'auto',
-						// 'bottom': 'auto',
-						// 'width': 'auto',
-					});
-				}
 			}
-		});
+
+			if (T1 > T2 && B1 <= B2 ) {
+				stk.css({
+					'position': 'relative',
+					'top': T1 - T2 + 'px',
+				});
+			} else if (B1 > B2){
+				stk.css({
+					'position': 'relative',
+					'top': parentH - (stkH + stkMB) - (stkOT - parentOT) + 'px',
+				});
+			} else {
+				stk.css({
+					'position': 'static',
+					'top': 'auto',
+				});
+			}
+		}
+		updateSticky();
+
+		winScroll(function () {
+			updateSticky();
+		})
 	};
 
 	winLoad(function () {
