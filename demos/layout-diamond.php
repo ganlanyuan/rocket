cce0222489307b1cb3bddab760017bc1bc293eb3
ganlanyuan <ganlanyuan@gmail.com>
2015-11-10 23:21:50 -0500

    update diamond layout

--- demos/layout-diamond.php
+++ demos/layout-diamond.php
@@ -5,25 +5,45 @@
 
     <div class="topic">
       <h2 class="main-heading"><span>layout: </span>diamond</h2>
-      <ul class="example-diamond" data-margin>
-        <li><div class="diamond" data-content><div class="diamond-content"><span>content</span></div></div></li>
-        <li><div class="diamond" data-content><div class="diamond-content"><span>content</span></div></div></li>
-        <li><div class="diamond" data-content><div class="diamond-content"><span>content</span></div></div></li>
-        <li><div class="diamond" data-content><div class="diamond-content"><span>content</span></div></div></li>
-        <li><div class="diamond" data-content><div class="diamond-content"><span>content</span></div></div></li>
-        <li><div class="diamond" data-content><div class="diamond-content"><span>content</span></div></div></li>
-        <li><div class="diamond" data-content><div class="diamond-content"><span>content</span></div></div></li>
-        <li><div class="diamond" data-content><div class="diamond-content"><span>content</span></div></div></li>
-        <li><div class="diamond" data-content><div class="diamond-content"><span>content</span></div></div></li>
-        <li><div class="diamond" data-content><div class="diamond-content"><span>content</span></div></div></li>
-        <li><div class="diamond" data-content><div class="diamond-content"><span>content</span></div></div></li>
-        <li><div class="diamond" data-content><div class="diamond-content"><span>content</span></div></div></li>
-        <li><div class="diamond" data-content><div class="diamond-content"><span>content</span></div></div></li>
-        <li><div class="diamond" data-content><div class="diamond-content"><span>content</span></div></div></li>
-        <li><div class="diamond" data-content><div class="diamond-content"><span>content</span></div></div></li>
-        <li><div class="diamond" data-content><div class="diamond-content"><span>content</span></div></div></li>
-        <li><div class="diamond" data-content><div class="diamond-content"><span>content</span></div></div></li>
-      </ul>
+      <div class="example-diamond">
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/1.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/2.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/3.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/4.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/5.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/6.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/7.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/8.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/9.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/10.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/11.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/12.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/1.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/2.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/3.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/4.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/5.jpg" alt=""></span></div></div>
+      </div>
+      <br>
+      <div class="example-diamond">
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/1.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/2.jpg" alt=""></span></div></div>
+        <div class="diamond-m"><div class="diamond-content" data-content><span><img src="images/diamond/3.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/4.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/5.jpg" alt=""></span></div></div>
+        <div class="diamond-m"><div class="diamond-content" data-content><span><img src="images/diamond/6.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/7.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/8.jpg" alt=""></span></div></div>
+        <div class="diamond-m"><div class="diamond-content" data-content><span><img src="images/diamond/9.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/10.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/11.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/12.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/1.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/2.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/3.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/4.jpg" alt=""></span></div></div>
+        <div class="diamond-s"><div class="diamond-content" data-content><span><img src="images/diamond/5.jpg" alt=""></span></div></div>
+      </div>
       <pre><code class="language-scss">
 .example-gallery { @include gallery(3); }
       </code></pre>
