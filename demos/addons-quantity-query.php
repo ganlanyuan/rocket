30887ab0588482faa2f08f6868f74e7b9a87f723
WilliamLin <ganlanyuan@gmail.com>
2015-11-12 18:22:36 -0500

    update quantity-query

--- demos/addons-quantity-query.php
+++ demos/addons-quantity-query.php
@@ -6,6 +6,16 @@
     <div class="topic">
       <h2 class="main-heading"><span>addons: </span>quantity-query</h2>
       <div class="quantity-query">
+        <div class="control">
+          <input type="button" value="+" class="plus">
+          <input type="button" value="-" class="minus">
+        </div>
+        <div class="at-least">
+          <div class="box"></div>
+          <div class="box"></div>
+          <div class="box"></div>
+          <div class="box"></div>
+        </div>
       </div>
       <pre><code class="language-scss">
       </code></pre>
