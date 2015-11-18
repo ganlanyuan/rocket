0de9aa584607b0b7d7d2903791df9d3a24f66676
WilliamLin <ganlanyuan@gmail.com>
2015-11-17 19:28:19 -0500

    add responsive-table

---
+++ demos/components-responsive-table.php
@@ -0,0 +1,96 @@
+<?php include 'include/head.php'; ?>
+<body>
+<div class="page">
+  <div class="container">
+
+    <div class="topic">
+      <h2 class="main-heading"><span>components: </span>responsive-table</h2>
+      <table class="table-1">
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Job Title</th>
+            <th>Favorite Color</th>
+            <th>Wars or Trek?</th>
+            <th>Porn Name</th>
+            <th>Date of Birth</th>
+            <th>Dream Vacation City</th>
+            <th>GPA</th>
+            <th>Arbitrary Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>James</td>
+            <td>Matman</td>
+            <td>Chief Sandwich Eater</td>
+            <td>Lettuce Green</td>
+            <td>Trek</td>
+            <td>Digby Green</td>
+            <td>January 13, 1979</td>
+            <td>Gotham City</td>
+            <td>3.1</td>
+            <td>RBX-12</td>
+          </tr>
+          <tr>
+            <td>The</td>
+            <td>Tick</td>
+            <td>Crimefighter Sorta</td>
+            <td>Blue</td>
+            <td>Wars</td>
+            <td>John Smith</td>
+            <td>July 19, 1968</td>
+            <td>Athens</td>
+            <td>N/A</td>
+            <td>Edlund, Ben (July 1996).</td>
+          </tr>
+          <tr>
+            <td>Jokey</td>
+            <td>Smurf</td>
+            <td>Giving Exploding Presents</td>
+            <td>Smurflow</td>
+            <td>Smurf</td>
+            <td>Smurflane Smurfmutt</td>
+            <td>Smurfuary Smurfteenth, 1945</td>
+            <td>New Smurf City</td>
+            <td>4.Smurf</td>
+            <td>One</td>
+          </tr>
+          <tr>
+            <td>Cindy</td>
+            <td>Beyler</td>
+            <td>Sales Representative</td>
+            <td>Red</td>
+            <td>Wars</td>
+            <td>Lori Quivey</td>
+            <td>July 5, 1956</td>
+            <td>Paris</td>
+            <td>3.4</td>
+            <td>3451</td>
+          </tr>
+          <tr>
+            <td>Captain</td>
+            <td>Cool</td>
+            <td>Tree Crusher</td>
+            <td>Blue</td>
+            <td>Wars</td>
+            <td>Steve 42nd</td>
+            <td>December 13, 1982</td>
+            <td>Las Vegas</td>
+            <td>1.9</td>
+            <td>Under the couch</td>
+          </tr>
+        </tbody>
+        </table>
+      <pre><code class="language-scss">
+      </code></pre>
+    </div>
+    
+    <?php include "include/more-demos.php" ?>
+  </div>
+  <?php include "include/site-footer.php" ?>
+</div>
+</body>
+</html>
+
