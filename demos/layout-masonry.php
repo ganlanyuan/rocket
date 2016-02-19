c90b527228dda7395e323514d39e170296e2c6d8
WilliamLin <ganlanyuan@gmail.com>
2016-02-19 16:28:08 -0500

    add masonry

---
+++ demos/layout-masonry.php
@@ -0,0 +1,66 @@
+<?php include 'include/head.php'; ?>
+<body>
+<div class="page">
+  <div class="container">
+
+    <div class="topic">
+      <h2 class="main-heading"><span>layout: </span>masonry</h2>
+      <div class="example">
+        <div class="masonry-layout" data-margin>
+          <div class="masonry-layout-panel">
+            <div class="masonry-layout-panel__content" data-content>"It was joyous because for the first time in three months, Marius and Ruth were together with all five children at the same time, in the same place. It was such a happy reunion, for everybody had so much to say," the website announced in an official statement on Thursday.</div>
+          </div>
+          <div class="masonry-layout-panel">
+            <div class="masonry-layout-panel__content" data-content>"Naomi couldn't stop talking; Ioan, the 2-year-old boy had learned a few more words and had a lot to say also. Baby Ezekiel was happier than ever," the statement continued.</div>
+          </div>
+          <div class="masonry-layout-panel">
+            <div class="masonry-layout-panel__content" data-content>After the family talked and played, they sat at the table to eat a meal.</div>
+          </div>
+          <div class="masonry-layout-panel">
+            <div class="masonry-layout-panel__content" data-content>The statement details how the family, which previously claimed that their children were taken because of concerns with their Christian faith, was allowed to pray and even sang a prayer-song before they ate their rooster and soup.</div>
+          </div>
+          <div class="masonry-layout-panel">
+            <div class="masonry-layout-panel__content" data-content>Although most of the visit was joyous, the parents grew concerned after Naomi told them that she didn't want them to die because she still has much to learn from them. Naomi's comment led them to wonder where she got the idea that they were going to die.</div>
+          </div>
+          <div class="masonry-layout-panel">
+            <div class="masonry-layout-panel__content" data-content>"The day was a mixture of joy and sorrow, for after three hours had passed, they had to part again," the statement explains. "The children cannot understand why they can't stay with their parents but, instead, have to go to strangers."</div>
+          </div>
+          <div class="masonry-layout-panel">
+            <div class="masonry-layout-panel__content" data-content>The Bodnarius still need to stand trial in order to regain custody of their children. As the agency wants them to submit to a psychological evaluation, the family has requested that a psychologist not associated with the Barnevernet to do the evaluations.</div>
+          </div>
+          <div class="masonry-layout-panel">
+            <div class="masonry-layout-panel__content" data-content>The statement claims that the prosecutor denied the family's proposal for an outside psychologist.</div>
+          </div>
+          <div class="masonry-layout-panel">
+            <div class="masonry-layout-panel__content" data-content>Although supporters believed that the Bodnarius would have their day in court sometime in March, the statement implies that the case might not be heard until May.</div>
+          </div>
+          <div class="masonry-layout-panel">
+            <div class="masonry-layout-panel__content" data-content>"The same prosecutor is too busy to attend a court hearing before the end of May and Barnevernet thinks this is in the superior interest of the baby," the statement argues.</div>
+          </div>
+          <div class="masonry-layout-panel">
+            <div class="masonry-layout-panel__content" data-content>While the Norwegian media has been less-than sympathetic toward the Bodnariu family, the children's grandfather <a href="http://bodnariufamily.org/articles/ruths-father-letter-for-local-government-and-norwegian-media-tatal-lui-ruth-mesaj-pentru-administratia-locala-si-presa-norvegiana-bilingual/" target="_blank">released a statement</a>&nbsp;on Friday to break his silence about the situation.</div>
+          </div>
+          <div class="masonry-layout-panel">
+            <div class="masonry-layout-panel__content" data-content>"This case has hurt the whole family and way beyond us. It has also been painful to read articles about the case in different newspapers where the upbringing of the children has been presented negatively without knowing the actual conditions," Ruth's father was quoted as saying. "We who live here in the home and have had the children around us since they were born, must be the ones who know best how the children have been with their parents."</div>
+          </div>
+          <div class="masonry-layout-panel">
+            <div class="masonry-layout-panel__content" data-content>"Grandmother has at times taken much care of the children. When they came from school and the parents were not at home, the kids were with us," he continued. "When their present home was being refurbished, the whole family lived with us for six months, and we, thereby, had full insight into their upbringing."</div>
+          </div>
+          <div class="masonry-layout-panel">
+            <div class="masonry-layout-panel__content" data-content>The grandfather added that he has never seen any violence in their family.</div>
+          </div>
+          <div class="masonry-layout-panel">
+            <div class="masonry-layout-panel__content" data-content>"We can assure that we have never seen that violence has been used against the children. Not even that they have raised their voices to them," he said. "The children themselves have never told us that the parents have been nasty to them. For us who love our grandchildren so much, it would be inconceivable to know that they were not happy and not to intervene. After all, we are well informed people."</div>
+          </div>
+        </div>
+      </div>
+      <pre><code class="language-scss">
+      </code></pre>
+    </div>
+    
+    <?php include "include/more-demos.php" ?>
+  </div>
+  <?php include "include/site-footer.php" ?>
+</div>
+</body>
+</html>
