da943f51c8b35f71a3583e48e65d93cb874e9dea
William <ganlanyuan@gmail.com>
2016-07-23 22:47:34 -0400

    test grid

--- tests/nightwatch/js/layout-grid.js
+++ tests/nightwatch/js/layout-grid.js
@@ -1,20 +1,17 @@
 module.exports = {
-  'Simple list grid' : function (client) {
+  'List grid' : function (client) {
     var url = client.launch_url + 'layout-grid.php';
     var containerWidth, 
+        verticalLocation,
         GUTTER = 20, 
         COLUMNS = [3, 4],
-        TOTAL = 0;
-
-    for (var i = COLUMNS.length; i--;) {
-      TOTAL += COLUMNS[i];
-    }
+        TOTAL = 7;
 
     client
       .url(url)
-      .resizeWindow(1400, 800)
       .pause(1000);
 
+    // check grid width
     client.getElementSize('#grid-simple-list', function (result) {
       containerWidth = result.value.width;
     });
@@ -25,24 +22,113 @@ module.exports = {
       this.verify.equal(result.value.width, thisWidth);
     });
 
-    client
-      .resizeWindow(600, 400);
-      
-    client.getElementSize('#grid-simple-list', function (result) {
-      containerWidth = result.value.width;
-    });
-
-
     client.getElementSize('#grid-simple-list > div:nth-child(2)', function (result) {
       var thisWidth = Math.round((containerWidth + GUTTER) * COLUMNS[1] / TOTAL - GUTTER);
 
       this.verify.equal(result.value.width, thisWidth);
     });
 
+    // check vertical location
+    client.getLocation('#grid-simple-list', function (result) {
+      verticalLocation = result.value.y;
+    });
+
+    client.getLocation('#grid-simple-list > div:nth-child(1)', function (result) {
+      this.verify.equal(result.value.y, verticalLocation);
+    });
+
+    client.getLocation('#grid-simple-list > div:nth-child(2)', function (result) {
+      this.verify.equal(result.value.y, verticalLocation);
+    });
+
+  },
+
+  'List grid with customized child elements' : function (client) {
+    var url = client.launch_url + 'layout-grid.php';
+    var containerWidth, 
+        verticalLocation,
+        GUTTER = 20, 
+        COLUMNS = [3, 4],
+        TOTAL = 7;
+
+    // check grid width
+    client.getElementSize('#grid-simple-list-child', function (result) {
+      containerWidth = result.value.width;
+    });
+
+    client.getElementSize('#grid-simple-list-child > .child:nth-child(1)', function (result) {
+      var thisWidth = Math.round((containerWidth + GUTTER) * COLUMNS[0] / TOTAL - GUTTER);
+
+      this.verify.equal(result.value.width, thisWidth);
+    });
+
+    client.getElementSize('#grid-simple-list-child > .child:nth-child(2)', function (result) {
+      var thisWidth = Math.round((containerWidth + GUTTER) * COLUMNS[1] / TOTAL - GUTTER);
+
+      this.verify.equal(result.value.width, thisWidth);
+    });
+
+    // check vertical location
+    client.getLocation('#grid-simple-list-child', function (result) {
+      verticalLocation = result.value.y;
+    });
+
+    client.getLocation('#grid-simple-list-child > .child:nth-child(1)', function (result) {
+      this.verify.equal(result.value.y, verticalLocation);
+    });
+
+    client.getLocation('#grid-simple-list-child > .child:nth-child(2)', function (result) {
+      this.verify.equal(result.value.y, verticalLocation);
+    });
+
+  },
+
+  'List grid with direction: right to left' : function (client) {
+    var url = client.launch_url + 'layout-grid.php';
+    var containerWidth, 
+        verticalLocation,
+        horizontalLocation1,
+        horizontalLocation2,
+        GUTTER = 20, 
+        COLUMNS = [3, 4],
+        TOTAL = 7;
+
+    // check grid width
+    client.getElementSize('#grid-simple-list-direction', function (result) {
+      containerWidth = result.value.width;
+    });
+
+    client.getElementSize('#grid-simple-list-direction > div:nth-child(1)', function (result) {
+      var thisWidth = Math.round((containerWidth + GUTTER) * COLUMNS[0] / TOTAL - GUTTER);
+
+      this.verify.equal(result.value.width, thisWidth);
+    });
+
+    client.getElementSize('#grid-simple-list-direction > div:nth-child(2)', function (result) {
+      var thisWidth = Math.round((containerWidth + GUTTER) * COLUMNS[1] / TOTAL - GUTTER);
+      horizontalLocation1 = thisWidth + GUTTER;
+
+      this.verify.equal(result.value.width, thisWidth);
+    });
+
+    // check vertical & horizontal location
+    client.getLocation('#grid-simple-list-direction', function (result) {
+      verticalLocation = result.value.y;
+      horizontalLocation1 += result.value.x;
+      horizontalLocation2 = result.value.x;
+    });
+
+    client.getLocation('#grid-simple-list-direction > div:nth-child(1)', function (result) {
+      this.verify.equal(result.value.y, verticalLocation);
+      this.verify.equal(result.value.x, horizontalLocation1);
+    });
+
+    client.getLocation('#grid-simple-list-direction > div:nth-child(2)', function (result) {
+      this.verify.equal(result.value.y, verticalLocation);
+      this.verify.equal(result.value.x, horizontalLocation2);
+    });
+
     client.end();
   }
-  // write : function (results, options, done) {
-  //   console.log(results);
-  //   done();
-  // }
+
 };
