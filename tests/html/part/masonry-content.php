9f294fef2845edc34beba23cddab5fccfccae99c
William Lin 22 <ganlanyuan@gmail.com>
2016-09-14 16:39:29 -0400

    update masonry

---
+++ tests/html/part/masonry-content.php
@@ -0,0 +1,80 @@
+<li class="masonry-item">
+  <div>
+    <div>
+      <img class="cluster-img" src="http://www.freeimageslive.com/galleries/food/fruitveg/preview/mange_toutes.jpg" alt="">
+    </div>
+  </div>
+</li>
+<li class="masonry-item">
+  <div class="masonry-content">
+    <h3>Jeb Bush Bows Out of Campaign, Humbled and Outmaneuvered</h3>
+    <p> I don’t remember Justice Scalia’s ever missing an argument in my eight years of covering the Supreme Court. He must have been recused occasionally, or sick, or stuck somewhere given his near-constant travels. But I can’t imagine what a Supreme Court argument will be like without him. </p>
+  </div>
+</li>
+<li class="masonry-item"><img src="http://www.freeimageslive.com/galleries/nature/coastline/preview/stones2131.jpg" alt=""></li>
+<li class="masonry-item">
+  <div class="masonry-content">
+    <h3>Delegate Count Leaving Bernie Sanders With Steep Climb</h3>
+    <p>Carl Hulse is the chief Washington correspondent of The Times. He has covered Congress and Washington politics for more than three decades. </p>
+  </div>
+</li>
+<li class="masonry-item"><img src="http://www.freeimageslive.com/galleries/sports/sportsgames/preview/soccer_ball.jpg" alt=""></li>
+<li class="masonry-item">
+  <div class="masonry-content">
+    <h3>The Party of Bush Yields, Warily, to a New Face: Donald Trump</h3>
+    <p>Adam Liptak has covered the Supreme Court for The Times since 2008. He’s a Yale-educated lawyer who practiced law for 14 years before becoming a journalist. </p>
+  </div>
+</li>
+<li class="masonry-item"><img src="http://www.freeimageslive.com/galleries/backdrops/colourful/pics/puzzel1002.jpg" alt=""></li>
+<li class="masonry-item">
+  <div class="masonry-content">
+    <h3>Pivotal Nursing Home Suit Raises a Simple Question: Who Signed the Contract?</h3>
+    <p>Keep everything secret. That’s the biggest thing the White House will try to do this week during its consideration of a replacement for Justice Scalia on the Supreme Court. </p>
+  </div>
+</li>
+<!-- <li class="masonry-item">
+  <img src="http://www.freeimageslive.com/galleries/objects/watch/preview/wall_clock.jpg" alt="">
+</li>
+<li class="masonry-item">
+  <div class="masonry-content">
+    <h3>Where Will Bush Voters Go?</h3>
+    <p>But if the White House has its way, all of that will happen directly under the noses of people like me, and without leaking. In 2009 and 2010, Mr. Obama’s advisers did that pretty well, though word of the president’s in-person interviews quickly spread. </p>
+  </div>
+</li>
+<li class="masonry-item">
+  <div>
+    <div><img class="cluster-img" src="http://www.freeimageslive.com/galleries/sports/lifestyle/preview/mountainbike_gears_8064585.jpg" alt=""></div>
+    <div class="masonry-content">
+      <p> On Monday, Justice Scalia’s chair and the bench in front of it will be draped in black. The arguments, in a pair of minor cases, will no doubt be muted. Some losses start to feel real only when you return to the routines of daily life. </p>
+    </div>
+  </div>
+</li>
+<li class="masonry-item">
+  <div class="masonry-content">
+    <h3>Donald Trump Wins South Carolina Primary; Cruz and Rubio Vie for 2nd</h3>
+    <p>This time, with the stakes so high, I expect my fellow White House correspondents and I will try hard to pierce the wall of confidentiality that Mr. Obama and his aides have already begun to erect. </p>
+  </div>
+</li>
+<li class="masonry-item"><img src="http://www.freeimageslive.com/galleries/food/drinks/preview/cappuccino_mug.jpg" alt=""></li>
+<li class="masonry-item">
+  <div class="masonry-content">
+    <h3>Apple Moves to Shift Battle Over Unlocking iPhone to Capitol Hill</h3>
+    <p>Is the Supreme Court hanging in the balance a winning campaign issue? Senator Ted Cruz sure thinks so. He has spent much of the last week assuring voters that he’s the only conservative fit to appoint a true “constitutionalist” to replace Justice Scalia. </p>
+  </div>
+</li>
+<li class="masonry-item"><img src="http://www.freeimageslive.com/galleries/workplace/energy/preview/arc_welding.jpg" alt=""></li>
+<li class="masonry-item">
+  <div class="masonry-content">
+    <h3>Oscars Pin Hopes on Chris Rock for Ratings</h3>
+    <p>Michael D. Shear, a longtime political reporter, is based at the White House for The Times. He focuses on immigration, health care, gun control and executive authority. </p>
+  </div>
+</li>
+<li class="masonry-item"><img src="http://www.freeimageslive.com/galleries/buildings/london/preview/big_ben.jpg" alt=""></li>
+<li class="masonry-item">
+  <div class="masonry-content">
+    <h3>5G Is a New Frontier for Mobile Carriers and Tech Companies</h3>
+    <p>It gets a little more personal on Monday as the Senate returns after a week’s recess. Now the conflict over how to replace Justice Scalia switches from one waged long distance through news releases and media interviews to one that is more face-to-face as senators converge on the floor and in Capitol hallways. </p>
+  </div>
+</li>
+<li class="masonry-item"><img src="http://www.freeimageslive.com/galleries/space/moon/pics/a11_h_37_5545.gif" alt=""></li>
+ -->
