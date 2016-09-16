86107728068db8709898f1c42a80ab2a89ef17f0
William Lin 22 <ganlanyuan@gmail.com>
2016-09-16 14:21:33 -0400

    update metro

--- tests/html/layout/metro.php
+++ tests/html/layout/metro.php
@@ -5,81 +5,54 @@
 
     <div class="topic">
       <h2 class="main-heading"><span>layout: </span>Metro</h2>
-      <div class="ovh metro-wrapper">
-        <ul class="outline" data-margin>
-          <li>
-            <div class="metro-item">
-              <ul class="g-1">
-                <li>
-                  <div class="metro-item" data-content=""><strong>A</strong><small>-1</small></div>
-                </li>
-                <li>
-                  <div class="metro-item" data-content=""><strong>A</strong><small>-2</small></div>
-                </li>
-                <li>
-                  <div class="metro-item" data-content=""><strong>A</strong><small>-3</small></div>
-                </li>
-                <li>
-                  <div class="metro-item" data-content=""><strong>A</strong><small>-4</small></div>
-                </li>
-                <li>
-                  <div class="metro-item" data-content=""><strong>A</strong><small>-5</small></div>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li>
-            <div class="metro-item">
-              <ul class="g-2">
-                <li>
-                  <div class="metro-item" data-content=""><strong>B</strong><small>-1</small></div>
-                </li>
-                <li>
-                  <div class="metro-item" data-content=""><strong>B</strong><small>-2</small></div>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li>
-            <div class="metro-item">
-              <ul class="g-3">
-                <li>
-                  <div class="metro-item" data-content=""><strong>C</strong><small>-1</small></div>
-                </li>
-                <li>
-                  <div class="metro-item" data-content=""><strong>C</strong><small>-2</small></div>
-                </li>
-                <li>
-                  <div class="metro-item" data-content=""><strong>C</strong><small>-3</small></div>
-                </li>
-                <li>
-                  <div class="metro-item" data-content=""><strong>C</strong><small>-4</small></div>
-                </li>
-                <li>
-                  <div class="metro-item" data-content=""><strong>C</strong><small>-5</small></div>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li>
-            <div class="metro-item">
-              <ul class="g-4">
-                <li>
-                  <div class="metro-item" data-content=""><strong>D</strong><small>-1</small></div>
-                </li>
-                <li>
-                  <div class="metro-item" data-content=""><strong>D</strong><small>-2</small></div>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li>
-            <div class="metro-item" data-content><strong>E</strong></div>
-          </li>
-          <li>
-            <div class="metro-item" data-content><strong>F</strong></div>
-          </li>
-        </ul>
+      <div class="ovh">
+        <div class="metro-list demo-container">
+          <ul data-margin>
+            <li><div class="metro-item" data-content></div></li>
+            <li><div class="metro-item" data-content></div></li>
+            <li><div class="metro-item" data-content></div></li>
+            <li><div class="metro-item" data-content></div></li>
+          </ul>
+        </div>
+        <div class="metro-child demo-container">
+          <ul data-margin>
+            <li class="child"><div class="metro-item" data-content></div></li>
+            <li class="child"><div class="metro-item" data-content></div></li>
+            <li class="child"><div class="metro-item" data-content></div></li>
+          </ul>
+        </div>
+        <div class="metro-ratio demo-container">
+          <ul data-margin>
+            <li><div class="metro-item" data-content></div></li>
+            <li><div class="metro-item" data-content></div></li>
+            <li><div class="metro-item" data-content></div></li>
+            <li><div class="metro-item" data-content></div></li>
+          </ul>
+        </div>
+        <div class="metro-breakpoint demo-container">
+          <ul data-margin>
+            <li><div class="metro-item" data-content></div></li>
+            <li><div class="metro-item" data-content></div></li>
+            <li><div class="metro-item" data-content></div></li>
+            <li><div class="metro-item" data-content></div></li>
+          </ul>
+        </div>
+        <div class="metro-breakpoint-condition demo-container">
+          <ul data-margin>
+            <li><div class="metro-item" data-content></div></li>
+            <li><div class="metro-item" data-content></div></li>
+            <li><div class="metro-item" data-content></div></li>
+            <li><div class="metro-item" data-content></div></li>
+          </ul>
+        </div>
+        <div class="metro-global-breakpoints demo-container">
+          <ul data-margin>
+            <li><div class="metro-item" data-content></div></li>
+            <li><div class="metro-item" data-content></div></li>
+            <li><div class="metro-item" data-content></div></li>
+            <li><div class="metro-item" data-content></div></li>
+          </ul>
+        </div>
       </div>
     </div>
     
