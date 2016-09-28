b367380b11f0507109f034c3a7b98053b1c1e7d0
William Lin 22 <ganlanyuan@gmail.com>
2016-09-28 10:10:02 -0400

    add shake styles into button

--- tests/html/components/button.php
+++ tests/html/components/button.php
@@ -7,19 +7,53 @@
       <h2 class="main-heading"><span>components: </span>buttons</h2>
       <div class="example">
         <div class="example-button">
-          <span class="normal">normal</span>
-          <a href="" class="radius">border-radius</a>
-          <span class="round">round</span> <br>
-          <span class="highlight">highlight</span>
-          <span class="slide">slide</span>
-          <button class="ripple">ripple</button>
-          <span class="cut">cut</span>
-          <span class="push" data-text="star"><span>push</span></span>
-          <span class="bubble">bubble</span>
-          <span class="shake">shake</span>
-          <span class="simple">simple</span>
-          <span class="veil">veil</span>
-          <span class="line-drawing">line-drawing <span></span><span></span><span></span><span></span></span>
+          <div class="default demo-container">
+            <button class="btn">default</button>
+          </div>
+          <div class="color demo-container">
+            <a href="" class="btn">color</a>
+          </div>
+          <div class="radius demo-container">
+            <span class="btn">radius</span>
+          </div>
+          <div class="round demo-container">
+            <div class="btn">round</div>
+          </div>
+          <div class="style-highlight demo-container">
+            <div class="btn">highlight</div>
+          </div>
+          <div class="style-simple demo-container">
+            <div class="btn">simple</div>
+          </div>
+          <div class="style-slide demo-container">
+            <div class="btn">slide</div>
+          </div>
+          <div class="style-ripple demo-container">
+            <div class="btn">ripple</div>
+          </div>
+          <div class="style-veil demo-container">
+            <div class="btn">veil</div>
+          </div>
+          <div class="style-cut demo-container">
+            <div class="btn">cut</div>
+          </div>
+          <div class="style-push demo-container">
+            <div class="btn" data-text="star"><span>push</span></div>
+          </div>
+          <div class="style-bubble demo-container">
+            <div class="btn">bubble</div>
+          </div>
+          <div class="style-line-drawing demo-container">
+            <div class="btn">line-drawing 
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+          <div class="style-shake demo-container">
+            <div class="btn">shake</div>
+          </div>
         </div>
       </div>
     </div>
@@ -27,12 +61,12 @@
   </div>
 </div>
 <script>
-  var ripple = document.querySelector('.ripple');
+  var ripple = document.querySelector('.style-ripple .btn');
   ripple.onclick = function () {
     if (ripple.className.indexOf('active') === -1) {
       ripple.className += ' active';
     } else {
-      ripple.className = 'ripple';
+      ripple.className = 'btn';
     }
   };
 </script>
