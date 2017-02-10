5c508c1466b08aa97ea91b090c3e58542aa9383d
William Lin 22 <ganlanyuan@gmail.com>
2017-02-10 18:03:48 -0500

    docs initial

--- gulpfile.js
+++ gulpfile.js
@@ -1,183 +1,211 @@
-var config = {
-  sassLang: 'libsass',
-  sourcemaps: 'sourcemaps',
-  libsass_options: {
-    outputStyle: 'expanded', 
-    precision: 7
-  },
-  rubysass_options: {
-    style: 'expanded', 
-    precision: 7,
-    sourcemap: true
-  },
-  server: {
-    base: '.',
-    hostname: '0.0.0.0',
-    keepalive: true,
-    stdio: 'ignore',
-  },
-  browserSync: {
-    proxy: '0.0.0.0:8000',
-    open: true,
-    notify: false
-  },
-  
-  // styles
-  sass: {
-    src: ['tests/html/scss/*.scss', 'tests/true/*.scss', 'demos/scss/*.scss'],
-    dest: ['tests/html/css', 'tests/true', 'demos/css'],
-  },
+const gulp = require('gulp');
+const path = require('path');
+const mergeStream = require('merge-stream');
+const browserSync = require('browser-sync').create();
+const rename = require('gulp-rename');
+const libsass = require('gulp-sass');
+const rubysass = require('gulp-ruby-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const modernizr = require('gulp-modernizr');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const svgmin = require('gulp-svgmin');
+const svgstore = require('gulp-svgstore');
+const inject = require('gulp-inject');
+const uncss = require('gulp-uncss');
+const nunjucks = require('gulp-nunjucks');
+const htmltidy = require('gulp-htmltidy');
 
-  // scripts
-  js: {
-    src:["src/js/**/*.js", "src/js/base/*.js"],
-    name: ['kit.js', 'kit.core.js'],
-    dest: 'dist',
-    options: {
-      // mangle: false,
-      output: {
-        quote_keys: true,
-      },
-      compress: {
-        properties: false,
-      }
-    }
-  },
-  
-  // watch
-  watch: {
-    php: '**/*.php',
-    html: '**/*.html'
-  },
+let sassLang = 'libsass';
+let sourcemapDest = '../sourcemaps';
+let PATHS = {
+  src: 'src/',
+  assets: 'assets/',
+  docs: 'docs/',
+  templates_docs: 'docs/templates/',
+  src_docs: 'docs/src/',
+  assets_docs: 'docs/assets/',
 };
-
-var gulp = require('gulp');
-var php = require('gulp-connect-php');
-var libsass = require('gulp-sass');
-var rubysass = require('gulp-ruby-sass');
-var sourcemaps = require('gulp-sourcemaps');
-
-var modernizr = require('gulp-modernizr');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var imagemin = require('gulp-imagemin');
-var svgstore = require('gulp-svgstore');
-var path = require('path');
-var svgmin = require('gulp-svgmin');
-var svg2png = require('gulp-svg2png');
-var colorize = require('gulp-colorize-svgs');
-var inject = require('gulp-inject');
-
-var browserSync = require('browser-sync').create();
-
-var rename = require('gulp-rename');
-var mergeStream = require('merge-stream');
+let NAMES = {
+  cssMain: 'main.css',
+  svgSprites: 'sprites.svg',
+};
+let scripts = {
+  src:[
+    'bower_components/go-native/dist/go-native.js',
+    'bower_components/sticky/dist/sticky.native.js',
+    'src/js/script.js'
+  ],
+  name: 'script.js',
+};
 
 function errorlog (error) {  
   console.error.bind(error);  
   this.emit('end');  
 }  
+function requireUncached( $module ) {
+  delete require.cache[require.resolve( $module )];
+  return require( $module );
+}
 
+// Nunjucks Task
+gulp.task('nunjucks', function() {
+  let data = requireUncached('./' + PATHS.templates_docs + 'data.json');
+  data.year = new Date().getFullYear();
+
+  let imageCount = 0;
+  data.getImageCount = function () { return imageCount += 1; };
+
+  return gulp.src(PATHS.templates_docs + '**/*.njk')
+    .pipe(nunjucks.compile(data), {
+      watch: true,
+      noCache: true,
+    })
+    .pipe(rename(function (path) { path.extname = ".html"; }))
+    .pipe(htmltidy({
+      doctype: 'html5',
+      wrap: 0,
+      hideComments: true,
+      indent: true,
+      'indent-attributes': false,
+      'drop-empty-elements': false,
+      'force-output': true
+    }))
+    .pipe(gulp.dest(PATHS.docs));
+});
+
+// Sass Task
 gulp.task('sass', function () {  
-  var tasks = [], 
-      srcs = config.sass.src,
-      dests = config.sass.dest;
+  return gulp.src(PATHS.src + 'scss/**/*.scss')  
+      .pipe(sourcemaps.init())
+      .pipe(libsass({
+        outputStyle: 'compressed', 
+        precision: 7
+      }).on('error', libsass.logError))  
+      .pipe(sourcemaps.write(sourcemapDest))
+      .pipe(gulp.dest(PATHS.assets + 'css'))
+      .pipe(browserSync.stream());
+});  
 
-  if (config.sassLang === 'libsass') {
-    for (var i = 0; i < srcs.length; i++) {
-      tasks.push(
-        gulp.src(srcs[i])  
-            .pipe(sourcemaps.init())
-            .pipe(libsass().on('error', libsass.logError))
-            .pipe(sourcemaps.write(config.sourcemaps))
-            .pipe(gulp.dest(dests[i]))
-      );
-    }
-  } else {
-    for (var i = 0; i < srcs.length; i++) {
-      tasks.push(
-        rubysass(srcs[i], config.rubysass_options)  
-            .pipe(sourcemaps.init())
-            .on('error', rubysass.logError)  
-            .pipe(sourcemaps.write(config.sourcemaps))
-            .pipe(gulp.dest(dests[i]))
-      );
-    }
-  }
+gulp.task('sass-docs', function () {  
+  return gulp.src(PATHS.src_docs + 'scss/**/*.scss')  
+      .pipe(sourcemaps.init())
+      .pipe(libsass({
+        outputStyle: 'compressed', 
+        precision: 7
+      }).on('error', libsass.logError))  
+      .pipe(sourcemaps.write(sourcemapDest))
+      .pipe(gulp.dest(PATHS.assets_docs + 'css'))
+      .pipe(browserSync.stream());
+});  
 
-  return mergeStream(tasks)
-        .pipe(browserSync.stream());
+gulp.task('svg-sprites', function () {
+  return gulp.src(PATHS.src_docs + 'svg/sprites/*.svg')
+    .pipe(svgmin(function (file) {
+      let prefix = path.basename(file.relative, path.extname(file.relative));
+      return {
+        plugins: [{
+          cleanupIDs: {
+            prefix: prefix + '-',
+            minify: true
+          }
+        }],
+        // js2svg: { pretty: true }
+      }
+    }))
+    .pipe(svgstore({ inlineSvg: true }))
+    .pipe(rename(NAMES.svgSprites))
+    .pipe(gulp.dest(PATHS.assets_docs + 'svg'));
 });
 
 // JS Task  
 gulp.task('js', function () {  
-  var tasks = [], 
-      srcs = config.js.src,
-      names = config.js.name;
+  if (scripts.name instanceof Array) {
+    let tasks = [], 
+        srcs = scripts.src,
+        names = scripts.name;
+        
+    for (let i = 0; i < srcs.length; i++) {
+      tasks.push(
+        gulp.src(srcs[i])
+            .pipe(sourcemaps.init())
+            .pipe(concat(names[i]))
+            .pipe(uglify({
+              // mangle: false,
+              output: {
+                quote_keys: true,
+              },
+              compress: {
+                properties: false,
+              }
+            }))
+            .on('error', errorlog)  
+            .pipe(sourcemaps.write(sourcemapDest))
+            .pipe(gulp.dest(PATHS.assets + 'js'))
+      );
+    }
 
-  for (var i = 0; i < srcs.length; i++) {
-    tasks.push(
-      gulp.src(srcs[i])
-          .pipe(sourcemaps.init())
-          .pipe(concat(names[i]))
-          .on('error', errorlog)  
-          .pipe(gulp.dest(config.js.dest))
-          .pipe(rename(names[i].replace('.js', '.min.js')))
-          .pipe(uglify(config.js.options))
-          .pipe(sourcemaps.write(config.sourcemaps))
-          .pipe(gulp.dest(config.js.dest))
-    );
-  }
-
-  return mergeStream(tasks)
+    return mergeStream(tasks)
         .pipe(browserSync.stream());
+
+  } else if(typeof scripts.name === 'string') {
+    return gulp.src(scripts.src)
+        .pipe(sourcemaps.init())
+        .pipe(concat(scripts.name))
+        .pipe(uglify())
+        .on('error', errorlog)  
+        .pipe(sourcemaps.write(sourcemapDest))
+        .pipe(gulp.dest(PATHS.assets + 'js'))
+        .pipe(browserSync.stream());
+  }
+});  
+
+// Inject Task
+gulp.task('inject', function () {
+  let svg4everybody = gulp.src('bower_components/svg4everybody/dist/svg4everybody.legacy.min.js')
+      .pipe(uglify());
+
+  return gulp.src(PATHS.templates + 'partials/layout.njk')
+      .pipe(inject(svg4everybody, {
+        starttag: '/* svg4everybody:js */',
+        endtag: '/* endinject */',
+        transform: function (filePath, file) {
+          return file.contents.toString().replace('height:100%;width:100%', '');
+        }
+      }))
+      .pipe(gulp.dest(PATHS.templates + 'partials'));
 });
 
-// Server
-gulp.task('server', function () {
-  php.server(config.server);
-});
-gulp.task('sync', ['server'], function() {
-  browserSync.init(config.browserSync);
-});
-
-// watch
-gulp.task('watch', function () {
-  gulp.watch(config.sass.src, function (e) {
-    var src = e.path.replace('/www/web/', ''), dest;
-    if (e.path.indexOf('html/') !== -1) {
-      dest = config.sass.dest[0];
-    } else if (e.path.indexOf('true/')) {
-      dest = config.sass.dest[1];
-    } else {
-      dest = config.sass.dest[2];
-    }
-    
-    if (config.sassLang === 'libsass') {
-      return gulp.src(src)  
-          .pipe(sourcemaps.init())
-          .pipe(libsass().on('error', libsass.logError))
-          .pipe(sourcemaps.write(config.sourcemaps))
-          .pipe(gulp.dest(dest))
-          .pipe(browserSync.stream());
-    } else {
-      return rubysass(src, config.rubysass_options)  
-          .pipe(sourcemaps.init())
-          .on('error', rubysass.logError)  
-          .pipe(sourcemaps.write(config.sourcemaps))
-          .pipe(gulp.dest(dest))
-          .pipe(browserSync.stream());
-    }
+// Server Task
+gulp.task('server', function() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    },
+    open: false,
+    notify: false
   });
-  gulp.watch(config.js.src, ['js']);
-  gulp.watch(config.watch.php).on('change', browserSync.reload);
-  gulp.watch(config.watch.html).on('change', browserSync.reload);
-})
+});
+
+// Watch Task
+gulp.task('watch', function () {
+  gulp.watch([PATHS.templates_docs + '**/*.njk', PATHS.templates + '*.json'], ['nunjucks']);
+  gulp.watch(PATHS.src_docs + 'scss/**/*.scss', ['sass-docs']);
+  gulp.watch(PATHS.src_docs + 'svg/sprites/*.svg', ['svg-sprites']);
+  gulp.watch(scripts.src, ['js']);
+  gulp.watch('**/*.html').on('change', browserSync.reload);
+});
 
 // Default Task
 gulp.task('default', [
+  // 'nunjucks', 
   // 'sass', 
-  // 'js',
-  'sync', 
+  // 'js', 
+  // 'move',
+  // 'svgmin', 
+  // 'svg-sprites',
+  // 'inject',
+  'server', 
   'watch',
-]); 
+]);  
